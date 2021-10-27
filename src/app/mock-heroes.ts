39a484f96ea85c461e7ebbd0b018a03b29952606
Martin Outumuro Quintas <martin.outumuroquinta@plexus.local>
2021-10-27 14:32:35 +0200

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+
+    { id: 1, name: 'Uno' },
+    { id: 2, name: 'Dos' },
+    { id: 3, name: 'tres' },
+    { id: 4, name: 'cuatro' },
+    { id: 5, name: 'cinco' },
+    { id: 6, name: 'seis' },
+    { id: 7, name: ' siete' },
+    { id: 8, name: 'ocho' },
+    { id: 9, name: 'nueve' },
+    { id: 10, name: 'diez' }
+];
